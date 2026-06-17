946dd36c7a5d3d5b957fd7681c574c79c856ec0e
Thabang <thabangkgobalale0@gmail.com>
2026-06-17 19:55:13 +0200

    update: added my finishing touches to about.html, index.html and products.html

--- js/main.js
+++ js/main.js
@@ -109,3 +109,39 @@ if (subscribeBtn) {
     });
 
 }
+const modal = document.getElementById("myModal");
+
+const openBtn = document.getElementById("openModal");
+
+const closeBtn = document.querySelector(".close");
+
+openBtn.onclick = function () {
+
+    modal.style.display = "block";
+
+}
+
+closeBtn.onclick = function () {
+
+    modal.style.display = "none";
+
+}
+
+window.onclick = function (event) {
+
+    if (event.target == modal) {
+
+        modal.style.display = "none";
+
+    }
+
+}
+function checkout() {
+
+    let message = "I want to order items from Peng Football T-Shirts.";
+
+    let url = "https://wa.me/27665256123?text=" + encodeURIComponent(message);
+
+    window.open(url, "_blank");
+}
+
